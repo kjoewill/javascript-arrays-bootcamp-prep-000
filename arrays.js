57aeeedfc03c3613781a6607e17563d277236603
kjoewill@gmail.com <kjoewill@gmail.com>
2018-07-05 21:57:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  console.log("")
+  console.log(">>>>>>>>>>>>>>>Test")
   return array.unshift(element)
 }
 
